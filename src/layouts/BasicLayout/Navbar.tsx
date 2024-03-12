0a9e6e9fd2e41a218e89b7dafb305c7793f313d1
sharebravery <sharebravery@gmail.com>
2024-03-12 13:16:12 +0800

    feat: rollbar

--- src/layouts/BasicLayout/Navbar.tsx
+++ src/layouts/BasicLayout/Navbar.tsx
@@ -1,7 +1,5 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import Logo from '/logo.png'
-import { useAccountEffect } from 'wagmi'
-import { BlastService } from '@/apis/blast'
 
 interface NavbarProps {
   title: string
@@ -9,13 +7,7 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = () => {
-  useAccountEffect({
-    async onConnect() {
-      const res = await BlastService.Authenticating()
-      console.log('%c🚀[Authenticating]-15:', 'color: #496d6d', res)
-    },
-  })
-
+  
   return (
     <nav className="h68 w-full flex items-center justify-between text-white" id="navBar">
       <div><img onClick={() => window.location.href = '/'} src={Logo} alt="" className="top-8 mx6 h44 w150 cursor-pointer max-sm:w-120" /></div>
--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,8 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import '@/locale/i18n.ts'
+import { Provider, ErrorBoundary } from '@rollbar/react'
 import { getLanguageLib } from './utils/getLanguageLib.ts'
-
 import { queryClient, walletCConfig } from './utils/connectWalletCustomChains.ts'
 import App from './App.tsx'
 import './index.css'
@@ -23,21 +23,33 @@ const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 const browserLanguageLib = getLanguageLib()
 
+const rollbarConfig = {
+  accessToken: 'cab2efcd5b4b414d8edf8199cf9ac6d7',
+  environment: 'testenv',
+}
+
+
+
 root.render(
-  <ConfigProvider
-    locale={browserLanguageLib.locale}
-    theme={{
-      algorithm: theme.darkAlgorithm,
-    }}
-  >
-    <BrowserRouter>
-      <WagmiProvider config={walletCConfig}>
-        <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider theme={darkTheme()} locale={browserLanguageLib.browserLanguage as Locale}>
-            <App />
-          </RainbowKitProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </BrowserRouter>
-  </ConfigProvider>,
+  <Provider config={rollbarConfig}>
+    <ErrorBoundary>
+      <ConfigProvider
+        locale={browserLanguageLib.locale}
+        theme={{
+          algorithm: theme.darkAlgorithm,
+        }}
+      >
+        <BrowserRouter>
+
+          <WagmiProvider config={walletCConfig}>
+            <QueryClientProvider client={queryClient}>
+              <RainbowKitProvider theme={darkTheme()} locale={browserLanguageLib.browserLanguage as Locale}>
+                <App />
+              </RainbowKitProvider>
+            </QueryClientProvider>
+          </WagmiProvider>
+        </BrowserRouter>
+      </ConfigProvider>
+    </ErrorBoundary>
+  </Provider>
 )
